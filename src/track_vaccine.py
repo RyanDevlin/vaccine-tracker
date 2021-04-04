fceff01b00dd0364e5bec160b2ed6d3021b5365e
Tyler Kobil <tylerkobil@Tylers-MacBook-Air.local>
2021-04-04 13:20:28 -0400

    setting up state mgr

--- src/track_vaccine.py
+++ src/track_vaccine.py
@@ -1,28 +1,29 @@
 from requests_html import HTMLSession
-import sys
 import time
+import os
 
+from src.state_manager import StateManager
 
 TURBO_VAX_URL = "https://www.turbovax.info/"
 
-def check_availability():
-    session = HTMLSession()
-    r = session.get(TURBO_VAX_URL)
-    r.html.render()
-    pg = r.html.text
-    availability = r.html.xpath('//*[@id="root"]/div/div[2]/div/div[3]/div[1]/div/div/div/div[2]/div/p')[0].full_text
-    print(availability)
+def poll_availability():
+    while True:
+        try:
+            session = HTMLSession()
+            response = session.get(TURBO_VAX_URL)
+            wait = response.html.render()
+            pg = response.html.text
+            availability = response.html.xpath('//*[@id="root"]/div/div[2]/div/div[3]/div[1]/div/div/div/div[2]/div/p')[0].full_text
+            print(availability)
+            # TODO - StateManager.set_state(state) (state = 'Available' or 'Not Available')
+            session.close()
+            time.sleep(15)
 
-    if 'Not Available' in availability:
-        return False
-    
-    return True
+        except KeyboardInterrupt:
+            session.close()
+            sys.exit()
+
 
 
 if __name__ == "__main__":
-    while True:
-        try:
-            check_availability()
-            time.sleep(5)
-        except KeyboardInterrupt:
-            sys.exit()
+    poll_availability()
