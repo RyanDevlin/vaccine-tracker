62d6c4e335b03790f5015f3704b452e7b1711da7
RyanDevlin <rdevlin.mail@gmail.com>
2021-04-04 17:29:46 -0400

    Working hashing

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ scrape turbovax site and alert when there is availability in NYC area
   python setup.py build 
   python setup.py install 
 ```
+# Install GeckoDriver on Linux
+```bash
+wget https://github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-macos.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
+```
+
+# Install GeckoDriver on MacOS
+```bash
+wget https://github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-macos.tar.gz
+tar -xvzf geckodriver*
+chmod +x geckodriver
+sudo mv geckodriver /usr/local/bin/
+```
--- src/state_manager.py
+++ src/state_manager.py
@@ -3,18 +3,22 @@
 class StateManager(object):
     """Class to Manage State in Memory"""
 
-    STATE = 'Not Available'
+    State = "b6ee60926c0a426addcbb7e087d4274498f35b1c" # Sha1 hash of the empty list, used as default
 
     @classmethod
-    def set_state(cls, state):
+    def set_state(cls, newState, handler):
         """Set State in memory and send alerts (email, smsg, etc)"""
-        if state not in ['Not Available', 'Available']:
-            raise RuntimeError('State {} Not an Option'.format(state))
+        if len(newState) != 40:
+            print("LENGTH: ", len(newState))
+            raise RuntimeError('State {} is not a hash legnth 40'.format(newState))
 
-        if state == 'Available' and cls.STATE != 'Available':
+        if newState != cls.State:
             # TODO - send email/smsg alert
-            pass
+            print("Detected new state! Sending email.")
+            handler()
+        else:
+            print("State is the same. No email to send.")
         
-        cls.STATE = state
+        cls.State = newState
         
 
